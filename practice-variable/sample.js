916ef2c0c833562f88de478a31f42e26b1a58372
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2026-01-07 10:14:47 +0000

    JavaScriptの変数宣言を学ぼう

---
+++ practice-variable/sample.js
@@ -0,0 +1,19 @@
+// varによる再宣言
+var nickname = "taro"
+console.log(nickname)
+var nickname = "ichiro"
+console.log(nickname)
+
+
+// varによる再代入
+var nickname = "taro"
+console.log(nickname)
+nickname = "jiro"
+console.log(nickname)
+
+// letによる再代入
+let nickname2 = "taro"
+console.log(nickname2)
+nickname2 = "jiro"
+console.log(nickname2)
+

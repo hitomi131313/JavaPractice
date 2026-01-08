fb4d32ea9fe1e4ea9cedcff11f475b6481a83249
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2026-01-08 10:33:09 +0000

    3章 【関数】確認問題

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ alert(promptStr);
 // じゃんけんの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
 
+while((user_hand != "グー") && (user_hand !="チョキ") && (user_hand !="パー") && (user_hand !=null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
+
 // じゃんけんの手をランダムに作成する関数を呼び出す
 let js_hand = getJShand();
 
@@ -37,7 +42,12 @@ let js_hand = getJShand();
 let judge = winLose(user_hand, js_hand);
 
 // 結果を表示する
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+if(user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else{
+  alert("またチャレンジしてね")
+}
+
 
 // ランダムでじゃんけんの手を作成する関数
 function getJShand(){
